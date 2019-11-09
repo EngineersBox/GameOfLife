b9e0eebc1bebd5391da9b4a7853a1c99ca986813
EngineersBox <toastcraft.info@gmail.com>
2019-11-09 13:26:39 +1100

    Added grid graph generator and added applyRules() function to Grid class (WIP)

--- js/grid.js
+++ js/grid.js
@@ -8,6 +8,17 @@ class Grid {
         this.cols = cols;
         this.rows = rows;
         this.grid = create2DArray(this.cols, this.rows);
+        this.surroundings = [
+            [-1, 1],
+            [0, 1],
+            [1, 1],
+            [-1, 0],
+            [1, 0],
+            [-1, -1],
+            [0, -1],
+            [1, -1]
+
+        ];
     }
 
     newGrid(value=0) {
@@ -17,6 +28,39 @@ class Grid {
     randomise() {
         // '~~' is shorthand for Math.floor()
         this.grid = this.grid.map(x => x.map(e => ~~(Math.random() * 2)));
+    }
+
+    applyRules() {
+        let tempGrid = this.grid.slice();
+        for (let i = 0; i < this.grid.length; i++) {
+            for (let j = 0; j < this.grid[0].length; j++) {
+                let count = 0;
+                for (let coord of this.surroundings) {
+                    if (this.state(coord[0], coord[1])) {
+                        count += this.state(coord[0], coord[1]);
+                    }
+                }
+                console.log(count);
+                if (this.grid[i][j] == 1) {
+                    if (count < 2 || count > 3) {
+                        tempGrid[i][j] = 0;
+                    }
+                } else {
+                    if (count == 3) {
+                        tempGrid[i][j] = 1;
+                    }
+                }
+            }
+        }
+        this.grid = tempGrid;
+    }
+
+    state(x, y) {
+        if (0 <= x && x < this.grid.length && 0 <= y && y < this.grid[0].length) {
+            return this.grid[x][y];
+        } else {
+            return null;
+        }
     }
 
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,23 @@
-const COLS = 10;
-const ROWS = 10;
+const COLS = 30;
+const ROWS = 30;
+const SQUARE_WIDTH = 20;
 
 let grid;
 
 function setup() {
+    let canvas = createCanvas(SQUARE_WIDTH * COLS, SQUARE_WIDTH * ROWS);
+    canvas.parent("sketch_view");
     grid = new Grid(COLS, ROWS);
 }
 
 function draw() {
+    for (let i = 0; i < grid.grid.length; i++) {
+        for (let j = 0; j < grid.grid[0].length; j++) {
+            let state = grid.state(i, j);
+            stroke(0);
+            fill(255 * state);
+            rect(i * SQUARE_WIDTH, j * SQUARE_WIDTH, SQUARE_WIDTH, SQUARE_WIDTH);
+        }
+    }
 
 }
